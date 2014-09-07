7f5d0c330a7acf071c2f61a97bd81af8c3c7d24f
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2014-09-07 11:43:45 +0200

    fix imports

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "http"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
-  http.
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	envPort := os.Getenv("PORT")
+	location := ":3000"
+	if len(envPort) > 0 {
+		location = ":" + envPort
+	}
+
+	http.ListenAndServe(location, nil)
 }
